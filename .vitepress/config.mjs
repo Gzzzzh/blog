c265103aa7e31251b420cff19f6bb67af992c896
zhihang <mac@macdeMacBook-Pro.local>
2024-12-16 17:55:11 +0800

    fix: 修改base

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,7 +3,7 @@ import { sidebar } from './sidebar'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/blog/',
+  base: '/',
   title: "Gzzzh blogs",
   description: "Gzzzh的前端学习",
   markdown: {
